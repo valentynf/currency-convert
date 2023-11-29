344e526fb00a2755684cedf44bece578fa445aca
valentynf <valentynf@wix.com>
2023-11-29 18:54:13 +0200

    removing some unnecessary files, minor index file updates

---
+++ src/components/CurrencyConverter/CurrencyConverter.tsx
@@ -0,0 +1,7 @@
+import styles from './CurrencyConverter.module.css';
+
+function CurrencyConverter() {
+  return <header className={styles['converter-container']}></header>;
+}
+
+export default CurrencyConverter;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return <header className={styles['header-container']}></header>;
+}
+
+export default Header;
